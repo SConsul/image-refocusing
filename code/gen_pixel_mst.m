f262c009cd36f16ae83676cd258d303f0fadc8e1
Parthasarathi Khirwadkar <kparth98@gmail.com>
2018-11-21 15:34:46 +0530

    adding gen_pixel_mst

--- code/gen_pixel_mst.m
+++ code/gen_pixel_mst.m
@@ -1,21 +1,22 @@
 function [pixel_mst,pixel_graph] = gen_pixel_mst(img)
     pixel_graph = graph();
-    pixel_graph = addnode(pixel_graph, size(img,1)*size(img,2));
-    node2nums = reshape(1:size(img,1)*size(img,2),size(img));
+    [m,n,~] = size(img);
+    pixel_graph = addnode(pixel_graph, m*n);
+    node2nums = reshape(1:m*n,[m,n]);
     dx = [1,0,1,1];
     dy = [0,1,1,-1];
-    [m,n] = size(img);
+    
     
     for i = 1:4
         a = node2nums(max(1,1-dx(i)):min(m-dx(i),m),max(1,1-dy(i)):min(n-dy(i),n));
         b = node2nums(max(1,1+dx(i)):min(m,m+dx(i)),max(1+dy(i),1):min(n,n+dy(i)));
         a = a(:);
         b = b(:);
-        weights = abs(img(a)-img(b));
+        weights = max([abs((img(a)-img(b))'),abs((img(a+m*n)-img(b+m*n))'),abs((img(a+2*m*n)-img(b+2*m*n))')]);
         pixel_graph = addedge(pixel_graph,a,b,weights);
     end
     
-%     p = plot(g,'EdgeLabel',g.Edges.Weight);
-    [pixel_mst,pred] = minspantree(pixel_graph);
+%     p = plot(pixel_graph,'EdgeLabel',pixel_graph.Edges.Weight);
+    [pixel_mst,~] = minspantree(pixel_graph);
 %     highlight(p,T);
 end
